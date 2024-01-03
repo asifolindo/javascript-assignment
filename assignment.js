b9e11e5e5807eafa4a87dde1e4d139f470c67579
asifolindo <olindoasif@gmail.com>
2024-01-03 19:12:02 +0600

    feetToMile Done

---
+++ assignment.js
@@ -0,0 +1,9 @@
+//#1 Convert any input value from feet into
+function feetToMile(feet){
+    var mile = feet / 5280;
+    return mile;
+}
+var resultMile = feetToMile(1000);
+console.log(resultMile, "Mile");
+
+// feetToMile Done.
